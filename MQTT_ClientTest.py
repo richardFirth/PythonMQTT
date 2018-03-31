d325343b1c1c3048881092c74d84dd7c8f4214eb
richardFirth <richardfirthucsb@gmail.com>
2018-03-31 13:52:51 -0700

    update

--- MQTT_ClientTest.py
+++ MQTT_ClientTest.py
@@ -1,13 +1,30 @@
 import paho.mqtt.client as mqtt
+import time
+
+#outTopic = "outTopic"
+inTopic = "inTopic"
+time.sleep(5)
+
 broker_address="192.168.1.117"
 print("Create New Instanace")
 
-      
 # http://www.steves-internet-guide.com/into-mqtt-python-client/
 
+client = mqtt.Client("P1") #create new instance
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+print("Subscribing to topic",inTopic)
+client.subscribe(inTopic)
+print("Publishing message to topic",inTopic)
 
-Client(client_id="Client1",clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
 
-client =mqtt.Client(client_name)
-
-connect(host, port=1883, keepalive=60, bind_address="")
+while True:
+    print ("RED")
+    client.publish(inTopic,"Rgb")
+    time.sleep(5)
+    print ("GREEN")
+    client.publish(inTopic,"rGb")
+    time.sleep(5)
+    print ("BLUE")
+    client.publish(inTopic,"rgB")
+    time.sleep(5)
